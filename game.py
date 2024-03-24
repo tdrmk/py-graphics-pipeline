5eb990eeb4c9f19d314226adcbd5a29ca621bcb6
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2024-03-23 23:38:06 -0700

    takes in external funcs to run in update and draw routines

--- game.py
+++ game.py
@@ -12,6 +12,14 @@ class Game:
         pygame.display.set_caption("Graphics Pipeline")
         self.clock = pygame.time.Clock()
         self.running = True
+        self.update_func = []
+        self.draw_func = []
+
+    def add_update_func(self, func):
+        self.update_func.append(func)
+
+    def add_draw_func(self, func):
+        self.draw_func.append(func)
 
     def run(self):
         while self.running:
@@ -27,9 +35,21 @@ class Game:
                 self.running = False
 
     def update(self):
-        pygame.display.set_caption(f"Graphics Pipeline - FPS: {self.clock.get_fps()}")
+        caption = f"Graphics Pipeline - FPS: {self.clock.get_fps():.2f}"
+        pygame.display.set_caption(caption)
+        # dt = seconds since last frame
+        dt = self.clock.get_time() / 1000.0
+        for func in self.update_func:
+            func(dt)
         pass
 
     def draw(self):
         self.screen.fill((0, 0, 0))
+        for func in self.draw_func:
+            func()
         pygame.display.flip()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
