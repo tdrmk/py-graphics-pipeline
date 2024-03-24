ad360d7bf58cd608293ad76a603bfb7073805ae5
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2024-03-24 01:02:23 -0700

    main program to test the simple graphics pipeline

---
+++ main.py
@@ -0,0 +1,21 @@
+from mesh import load_mesh_from_obj
+from graphics_pipeline import GraphicsPipeline
+from game import Game, WIDTH, HEIGHT
+from camera import Camera
+import numpy as np
+
+
+FOV = np.deg2rad(60)
+if __name__ == "__main__":
+    mesh = load_mesh_from_obj("models/cube_far.obj")
+    camera = Camera(FOV, WIDTH / HEIGHT, 0.1, 1000)
+    game = Game()
+    graphics_pipeline = GraphicsPipeline([mesh], camera, game.screen)
+    game.add_draw_func(graphics_pipeline.draw)
+
+    def rotate_mesh(dt):
+        mesh.rotation += np.array([1, 0, 0]) * dt
+
+    game.add_update_func(rotate_mesh)
+    game.add_update_func(lambda _: graphics_pipeline.update())
+    game.run()
