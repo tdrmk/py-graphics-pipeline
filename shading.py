5f554622d9c21319f3fe6cf733a5800b0c2ccaf9
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2024-03-25 22:51:24 -0700

    renamed from lighting to shader

--- shading.py
+++ shading.py
@@ -6,17 +6,21 @@ DIFFUSE_LIGHT = 0.6
 SPECULAR_LIGHT = 0.3
 SPECULAR_EXPONENT = 10
 
-# 3D lighting model
+# A pixel shader that calculates the color of the fragment (ie, pixel)
+# assumes a single directional light source
 # reference: https://www.youtube.com/watch?v=TEjDYtkLRdQ
 
 
-class Lighting:
+class PixelShader:
     def __init__(
         self, light_direction=np.array([0.0, 0.0, 1.0]), light_color=(255, 255, 255)
     ):
+        # the direction of the light source
         self.light_direction = light_direction
         self.light_color = light_color
 
+    # calculates the intensity of the lighting at the fragment,
+    # given the normal, position of the object and the camera
     def light_intensity(self, normal, object_position, camera_position):
         ambient = AMBIENT_LIGHT
         diffuse = DIFFUSE_LIGHT * max(0, np.dot(normal, -self.light_direction))
@@ -31,16 +35,23 @@ class Lighting:
         )
         return ambient, diffuse, specular
 
-    # a simple fragment shader that calculates the color of the fragment (ie, pixel)
-    def fragment_shader(
+    # calculates the actual color of the pixel after applying the lighting,
+    # given the ambient, diffuse, specular intensities and the texture color of the fragment
+    def get_color(self, ambient, diffuse, specular, color=(255, 255, 255)):
+        pixel_color = (
+            np.array(color) * (ambient + diffuse)
+            + np.array(self.light_color) * specular
+        )
+
+        return np.clip(pixel_color, 0, 255).astype(int)
+
+    # computes the color of the fragment (pixel) given the normal, position of the object,
+    # position of the camera and the texture color of the fragment (pixel)
+    def compute_color(
         self, normal, object_position, camera_position, texture_color=(255, 255, 255)
     ):
         ambient, diffuse, specular = self.light_intensity(
             normal, object_position, camera_position
         )
-        color = (
-            np.array(texture_color) * (ambient + diffuse)
-            + np.array(self.light_color) * specular
-        )
 
-        return np.clip(color, 0, 255).astype(int)
+        return self.get_color(ambient, diffuse, specular, texture_color)
